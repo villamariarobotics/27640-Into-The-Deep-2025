ef1acd2ebc5dab889649f3cbc36fd1b2cdbf4744
KeiraV66 <25vijhk@villamaria.qc.ca>
2025-01-30 16:01:01 -0500

    Updates to subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EndEffectorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EndEffectorSubsystem.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class EndEffectorSubsystem {
-}
+    static CRServo servo_motor;
+
+    public static void initialize (HardwareMap hwMap) {
+        servo_motor = hwMap.get(CRServo.class, "servo_motor");
+    }
+    public void turnMotors(float speed){
+        servo_motor.setPower(speed);
+    }
+    public void EndEffector_control (float speed){
+        servo_motor.setPower(speed);
+        telemetry.addData("servo_motor", "servo_speed");
+    }
+
+    }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PivotSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PivotSubsystem.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class PivotSubsystem {
+    static DcMotor pivot_motor;
+
+    public static void initialize(HardwareMap hwMap) {
+            pivot_motor = hwMap.get(DcMotor.class, "pivot_motor");
+            //pivot_motor.setDirection(DcMotorSimple.Direction.REVERSE);//
+            pivot_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void turnMotor(float speed){
+        pivot_motor.setPower(speed);
+    }
+    public void pivotSpeed(float left_speed){
+        pivot_motor.setPower(left_speed);
+        telemetry.addData("pivot_motor", "pivot_speed");
+    }
 }
